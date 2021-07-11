c3cbc515825455e3812cd502bb3f8a2518464f8f
Ghamad chand <ghamad@ghamad.onmicrosoft.com>
2021-07-11 06:47:18 +0000

    Updated hello-world.go

--- hello-world.go
+++ hello-world.go
@@ -1,5 +1,20 @@
 package main
+
 import "fmt"
+import "net/http"
+
+
+func index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Hello World</h1>")
+}
+
+func health(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Health Check Pass</h1>")
+}
+
 func main() {
-    fmt.Println("hello world")
+	http.HandleFunc("/",index)
+	http.HandleFunc("/health",health)
+	fmt.Println("Server Starting...")
+	http.ListenAndServe(":8080",nil)
 }
