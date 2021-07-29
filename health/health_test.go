9ff23126b48b6b79cb24e64861324cb3176a1118
Ghamad chand <ghamad@ghamad.onmicrosoft.com>
2021-07-29 23:58:20 +0000

    Updated health_test.go

--- health/health_test.go
+++ health/health_test.go
@@ -5,5 +5,15 @@ import "net/http/httptest"
 import "testing"
 
 func TestGetHealth(t *testing.T) {
-    
+    t.Run("returns healthy", func(t *testing.T) {
+        request, _ := http.NewRequest(http.MethodGet,"/health",nil)
+        response := httptest.NewRecorder()
+
+        health(response, request)
+        got := response.Body.String()
+        want := "healthy"
+        if gpt != want {
+            t.Errorf("Hanlder returned wrong message, got %q, want %q", got, want)
+        }
+    })
 }
