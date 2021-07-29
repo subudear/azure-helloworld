d0dea1c9bbe32461bfdcce367c4bbe6db66b2252
Ghamad chand <ghamad@ghamad.onmicrosoft.com>
2021-07-29 23:48:03 +0000

    Updated hello-world.go

--- hello-world.go
+++ hello-world.go
@@ -2,15 +2,12 @@ package main
 
 import "fmt"
 import "net/http"
-
+import "hello-world/health"
 
 func index(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Hello World</h1>")
 }
 
-func health(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Health Check Pass</h1>")
-}
 
 func main() {
 	http.HandleFunc("/",index)
