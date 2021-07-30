e2bb0bee65fd8216f807a986b048f34fef17a67a
Ghamad chand <ghamad@ghamad.onmicrosoft.com>
2021-07-30 00:22:27 +0000

    Updated health.go

--- health/health.go
+++ health/health.go
@@ -4,7 +4,7 @@ import "net/http"
 import "fmt"
 
 //check health
-func health(w http.ResponseWriter, r *http.Request) {
+func Health(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprintf(w, "<h1>Health Check Pass</h1>")
 }
