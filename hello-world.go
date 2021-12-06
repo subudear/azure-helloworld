cb589fffdfe612e5cdf661c4ece1bad9e9704f62
Ghamad chand <ghamad@ghamad.onmicrosoft.com>
2021-12-06 10:01:29 +0000

    Updated hello-world.go

--- hello-world.go
+++ hello-world.go
@@ -2,20 +2,20 @@ package main
 
 import "fmt"
 import "net/http"
-import "hello-world/health"
+
 
 func index(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Hello World</h1>")
 }
 
 
-func main() {
-	fmt.Println("Server Starting...")
-    mux := http.NewServeMux()
-    mux.HandleFunc("/", index)
-	//http.HandleFunc("/",index)
-	//http.HandleFunc("/health",health.health)
-	mux.HandleFunc("/health", health.Health)
+func health(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Service Health Check : Pass</h1>")
+}
 
+func main() {
+	http.HandleFunc("/",index)
+	http.HandleFunc("/health",health)
+	fmt.Println("Server Listening at 8080...")
 	http.ListenAndServe(":8080",nil)
 }
