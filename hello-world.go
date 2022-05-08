fba33b166921e4394b3b384305e31dd52a8eed66
Ghamad chand <ghamad@ghamad.onmicrosoft.com>
2022-05-08 05:06:51 +0000

    Updated hello-world.go

--- hello-world.go
+++ hello-world.go
@@ -5,7 +5,7 @@ import "net/http"
 
 
 func index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello World</h1>")
+	fmt.Fprintf(w, "<h1>Hello World - running image 706949302588.dkr.ecr.ap-southeast-2.amazonaws.com/testworld:#{Build.BuildId}#</h1>")
 }
 
 
